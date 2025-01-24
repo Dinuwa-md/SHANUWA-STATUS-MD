1da96d589cd43f96c7c284f919d34241f7922794
Dinuwa-md <dinuwamdnew2007@gmail.com>
2025-01-24 17:48:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session id",
+SESSION_ID: process.env.SESSION_ID || "KAVI-EXE=tph1hKZD#JoOscSShh5xZnsIFxX_7We1ApqX97PXA4VtjJsB0VkE",
 ALIVE_REACT: process.env.ALIVE_REACT || "👋",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://bit.ly/42qYxJO",
 ALIVE_MSG: process.env.ALIVE_MSG || "SHANUWA MD BOT ONLINE 🗣️",
@@ -14,7 +14,7 @@ MODE: process.env.MODE || "public",
 ALWAYS_ONLINE : process.env.ALWAYS_ONLINE || "false",
 AUTO_TYPING: process.env.AUTO_TYPING || "false",
 AUTO_RECORDING: process.env.AUTO_RECORDING || "true",
-AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
+AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "trye",
 AUTO_STATUS__MSG: process.env.AUTO_STATUS__MSG || "*මන් ඔයාගේ ස්ටේටස් එක ඉස්සෙල්ලම බැලුවා පැටියෝ 🧚‍♂️*",
 ANTI_CALL: process.env.ANTI_CALL || "false",
 };
