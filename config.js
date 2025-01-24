cbad056df8b3aa8dc99fc850cc540d38a45a446a
Dinuwa-md <dinuwamdnew2007@gmail.com>
2025-01-24 17:49:58 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "KAVI-EXE=tph1hKZD#JoOscSShh5xZnsIFxX_7We1ApqX97PXA4VtjJsB0VkE",
+SESSION_ID: process.env.SESSION_ID || "SHAHUWA-MD=tph1hKZD#JoOscSShh5xZnsIFxX_7We1ApqX97PXA4VtjJsB0VkE",
 ALIVE_REACT: process.env.ALIVE_REACT || "👋",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://bit.ly/42qYxJO",
 ALIVE_MSG: process.env.ALIVE_MSG || "SHANUWA MD BOT ONLINE 🗣️",
